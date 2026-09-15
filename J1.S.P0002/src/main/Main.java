4d5dc10d6c43194e5e0b363a6b2f8cacc2467051
ThanhHai <haingothak@gmail.com>
2026-09-16 03:45:25 +0700

    Update Selection Sort

--- J1.S.P0002/src/main/Main.java
+++ J1.S.P0002/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import utils.Validation;
+
 /**
  * Main class is used to run Selection Sort program.
  *
@@ -7,5 +9,28 @@ package main;
  * @author HaiNTHE191763
  */
 public class Main {
+    public static void main(String[] args) {
+        Validation validator = new Validation();
 
+        //Step 1: Input the number of elements in the array.
+        int size = validator.getInt("Enter number of array: ",
+                "Error: Number must be grater than 0!",
+                "Error: Invalid integer number. Please enter again: ",
+                1, Integer.MAX_VALUE);
+
+//        //Step 2: Initialize the array and generate random numbers.
+//        BubbleSort bubbleSortObj = new BubbleSort(size);
+//        bubbleSortObj.generateArray();
+//
+//        //Step 3: Display the array before sorting.
+//        System.out.println("Unsorted array: ");
+//        bubbleSortObj.display();
+//
+//        //Step 4: Sort the array using the Bubble Sort algorithm.
+//        bubbleSortObj.bubbleSort();
+//
+//        //Step 5: Display the array after sorting.
+//        System.out.println("Sorted array: ");
+//        bubbleSortObj.display();
+    }
 }
--- J1.S.P0002/src/model/SelectionSort.java
+++ J1.S.P0002/src/model/SelectionSort.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 /**
  * SelectionSort class provides initialization, generates random array, displays
  * and sorts array bay Selection Sort algorithm.
@@ -9,4 +11,42 @@ package model;
  */
 public class SelectionSort {
 
+    private int[] array;
+
+    /**
+     * Initialize the array with the specified size.
+     *
+     * @param size size of array
+     */
+    public SelectionSort(int size) {
+        array = new int[size];
+    }
+
+    /**
+     * Generate random array.
+     */
+    public void generateArray() {
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(array.length);
+        }
+    }
+
+    /**
+     * Display array on screen.
+     */
+    public void display() {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public void selectionSort() {
+
+    }
 }
